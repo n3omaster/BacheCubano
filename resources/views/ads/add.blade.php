c8692a2ecf838948a1eddd6147c560e13672f96c
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-03-26 13:27:37 -0400

    nl2br ads

--- resources/views/ads/add.blade.php
+++ resources/views/ads/add.blade.php
@@ -127,7 +127,7 @@
                                 <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
 
-                                <textarea name="description" class="form-control @error('description') is-invalid @enderror" rows="8" style="resize: vertical">@if($edit){!! $ad->description->description !!}@else{!! old('description') !!}@endif</textarea>
+                                <textarea name="description" class="form-control @error('description') is-invalid @enderror" rows="8" style="resize: vertical">@if($edit){!! nl2br($ad->description->description) !!}@else{!! old('description') !!}@endif</textarea>
 
                                 <!--
                                     <textarea name="description" class="form-control @error('description') is-invalid @enderror" rows="8" style="resize: vertical">@if($edit){!! $ad->description->description !!}@else{!! old('description') !!}@endif</textarea>
