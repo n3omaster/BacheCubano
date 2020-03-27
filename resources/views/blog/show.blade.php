aefc91770971d5bd52261897f79c6b89f7feb4f1
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-03-27 17:36:24 -0400

    Disqus comments

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -24,7 +24,6 @@
             <div class="col-lg-8 col-md-12 col-xs-12">
                 <!-- Start Post -->
                 <div class="blog-post single-post">
-
                     <!-- Post thumb -->
                     <div class="post-thumb">
                         <a href="{{ URL::current() }}">
@@ -58,11 +57,33 @@
                     <!-- Post Content -->
                 </div>
                 <!-- End Post -->
+                <div id="disqus_thread"></div>
+
             </div>
             @include('blog.sidebar')
         </div>
     </div>
 </div>
 <!-- End Content -->
+
+@push('script')
+<script>
+    /**
+     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    var disqus_config = function() {
+        this.page.url = "{{ URL::current(); }}"; // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = "{{ $blog_post->id }}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document,
+            s = d.createElement('script');
+        s.src = 'https://bachecubano.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comentarios vía Disqus.</a></noscript>
+@endpush
 
 @endsection
