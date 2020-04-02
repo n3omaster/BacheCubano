5a2ed13fba2e1bc8b707a360f46e4de2c2f4e171
Erich Garcia Cruz <ecruz@bachecubano.com>
2020-04-02 16:29:27 -0400

    h1

--- resources/views/puedosalir.blade.php
+++ resources/views/puedosalir.blade.php
@@ -3,7 +3,20 @@
 @section('content')
 
 @section('hero')
-@include('blocks.hero')
+<!-- Hero Area Start -->
+<div id="hero-area">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-9 col-lg-9 col-xs-12 text-center">
+                <div class="contents">
+                    <h1 class="head-title">¿Puedo salir a la calle?</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Hero Area End -->
 @endsection
 
 <h1 style="font-size: 300px; padding-top: 300px;" class="text-center">NO!</h1>
